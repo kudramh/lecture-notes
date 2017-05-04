cc62d805beb0097a52cef14143fa6a702b6abfd8
Marduk <kudramh@gmail.com>
2017-05-03 19:15:52 -0500

    SpringCloud Microservices

---
+++ SpringCloud/bootrest-maven/src/test/java/org/kudram/BootrestApplicationTests.java
@@ -0,0 +1,16 @@
+package org.kudram;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootrestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
